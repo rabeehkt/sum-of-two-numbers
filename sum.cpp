53da9876ef4c762363a86a5bfd6d927bc1238cad
Rabeeh K T <35598587+rabeehkt@users.noreply.github.com>
2020-07-28 19:05:15 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int n1,n2,n3;
+ cout<<"Enter 1st Number: ";
+ cin>>n1;
+ cout<<"Enter 2nd Number: ";
+ cin>>n2;
+ n3=n1+n2;
+ cout<<"Sum="<<n3;
+}
